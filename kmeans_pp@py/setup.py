86f53f2ebfd7186fa2f9c1c5ae345038fa8df7c9
Daniel Bar Lev <danielbarlev@nova.cs.tau.ac.il>
2024-03-13 14:19:11 +0200

    setup

--- kmeans_pp@py/setup.py
+++ kmeans_pp@py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import Extension, setup
 
-module = Extension("geo_capi", sources=['geomodule.c'])
-setup(name='geo_capi',
+module = Extension("mykmeanssp", sources=['/a/home/cc/students/cs/danielbarlev/testrun/Software-project-HW2/kmean_pp@C/pd.c'])
+setup(name='mykmeanssp',
      version='1.0',
      description='Python wrapper for custom C extension',
      ext_modules=[module])
